02462a75de9bfac6bf92cc1371eb8360c1f9d748
sebasqr22 <63075293+sebasqr22@users.noreply.github.com>
2024-06-13 11:06:02 -0600

    Documentación

--- main.py
+++ main.py
@@ -1,35 +1,40 @@
 import os
 
-ruta = "/Users/sebastianqr.2208/Desktop/Arqui 1/PROYECTO 3/Iteraciones SPEC"
+ruta = "/Users/sebastianqr.2208/Desktop/Arqui 1/PROYECTO 3/Iteraciones SPEC/NonCachingSimpleCPU/Branch Predictor/"
 
-#funcion que tome la variable archivos de la funcion leer_archivos y reciba una lista de strings que se llame lista_buscar y busque cada string de lista_buscar en la matriz y devuelva esa linea de cada matriz
-def buscar_linea(archivos, lista_buscar):
-    lineas = []
-    for archivo in archivos:
-        for linea in archivo:
-            for string in lista_buscar:
-                if string in linea:
-                    lineas.append(linea)
-    return lineas
+#funcion que recibe una ruta de una carpeta, busca el archivo stats.txt. La funcio debe recibir una lista de palabras, cuando en el archivo encuentre una palabra, debe retornar toda la linea
+def buscar(ruta, palabras):
+    with open(ruta + "/stats.txt") as f:
+        for linea in f:
+            if any(palabra in linea for palabra in palabras):
+                print(linea)
 
-#funcion que toma la variable ruta y lee todos los archivos que hay en las carpetas dentro de la ruta, los archivos se llaman stats.txt y los retorna en una matriz. Ademas, debe de guardar el nombre de la carpeta que esta leyendo
-def leer_archivos():
-    archivos = []
-    for carpeta in os.listdir(ruta):
-        if os.path.isdir(os.path.join(ruta, carpeta)):
-            for archivo in os.listdir(os.path.join(ruta, carpeta)):
-                if archivo == "stats.txt":
-                    with open(os.path.join(ruta, carpeta, archivo), "r") as file:
-                        archivos.append([carpeta, file.readlines()])
-    return archivos
+#funcion que con la varibale ruta, busque el archivo runGEM5.sh  La unica entrada de la funcion es la ruta. Debe imprimir el contenido del archivo en forma de lista
+def buscar_parametro(ruta, parametros):
+    lista = []
+    with open(ruta + "/runGEM5.sh") as f:
+        for linea in f:
+            lista.append(linea)
+
+    #recorrer la lista en la posicion cuatro, hacer un .split para " " e imprimir la lista
+    nueva = lista[4].split(" ")
+    for i in nueva:
+        for parametro in parametros:
+            #si i contiene parte de la palabra parametro, imprimir i
+            if parametro in i:
+                print(i)
 
 
-#funcion main que llama a las funciones leer_archivos y buscar_linea y luego recorrera lo que retorna buscar_linea y lo imprimira
-def main():
-    archivos = leer_archivos()
-    lista_buscar = ["BTB"]
-    lineas = buscar_linea(archivos, lista_buscar)
-    for linea in lineas:
-        print(linea)
+def recorrer(lista, parametros):
+    for i in range(1,6):
 
-main()
+        ruta_nueva = ruta + str(i)
+        print(ruta_nueva)
+        buscar_parametro(ruta_nueva, parametros)
+        buscar(ruta_nueva, lista)
+        print("------------------------------------------------------------//------------------------------------------------------------\n\n\n")
+
+#lista = ['system.cpu.dcache.power_state.pwrStateResidencyTicks', 'system.cpu.icache.overallMisses']
+lista = ['dcache', 'icache']
+parametros = ['l1d_size', 'l1i_size']
+recorrer(lista, parametros)
